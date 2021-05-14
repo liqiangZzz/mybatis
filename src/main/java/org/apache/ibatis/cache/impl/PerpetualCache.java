b0a760d56989c9ae76055c77a01d8aed14086e52
邓澎波 <dengpbs@163.com>
2021-05-14 09:48:18 +0800

    源码注释后提交的第一版-dpb

---
+++ src/main/java/org/apache/ibatis/cache/impl/PerpetualCache.java
@@ -0,0 +1,97 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.cache.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.CacheException;
+
+/**
+ * 在装饰器模式用 用来被装饰的对象
+ * 缓存中的  基本缓存处理的实现
+ * 其实就是一个 HashMap 的基本操作
+ * @author Clinton Begin
+ */
+public class PerpetualCache implements Cache {
+
+  private final String id; // Cache 对象的唯一标识
+
+  // 用于记录缓存的Map对象
+  private final Map<Object, Object> cache = new HashMap<>();
+
+  public PerpetualCache(String id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public int getSize() {
+    return cache.size();
+  }
+
+  @Override
+  public void putObject(Object key, Object value) {
+    cache.put(key, value);
+  }
+
+  @Override
+  public Object getObject(Object key) {
+    return cache.get(key);
+  }
+
+  @Override
+  public Object removeObject(Object key) {
+    return cache.remove(key);
+  }
+
+  @Override
+  public void clear() {
+    cache.clear();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (getId() == null) {
+      throw new CacheException("Cache instances require an ID.");
+    }
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Cache)) {
+      return false;
+    }
+
+    Cache otherCache = (Cache) o;
+    // 只关心ID
+    return getId().equals(otherCache.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    if (getId() == null) {
+      throw new CacheException("Cache instances require an ID.");
+    }
+    // 只关心ID
+    return getId().hashCode();
+  }
+
+}
---
+++ src/main/java/org/apache/ibatis/reflection/property/PropertyTokenizer.java
@@ -0,0 +1,82 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.reflection.property;
+
+import java.util.Iterator;
+
+/**
+ * 属性分词器
+ *   orders[0].items[0].name
+ * @author Clinton Begin
+ */
+public class PropertyTokenizer implements Iterator<PropertyTokenizer> {
+  private String name; // 当前表达式的名称
+  private final String indexedName; // 当前表达式的索引名称
+  private String index; // 索引下标
+  private final String children; // 子表达式
+
+  /**
+   *  例如 ：orders[0].items[0].name
+   * @param fullname
+   */
+  public PropertyTokenizer(String fullname) {
+    int delim = fullname.indexOf('.'); // 查找 . 的位置
+    if (delim > -1) {
+      name = fullname.substring(0, delim); // 初始化 name = orders[0]
+      children = fullname.substring(delim + 1); // 初始化 children = items[0].name
+    } else { // 如果不包含 .
+      name = fullname;
+      children = null;
+    }
+    indexedName = name; // 初始化 indexedName  indexedName = orders[0]
+    delim = name.indexOf('[');
+    if (delim > -1) {
+      index = name.substring(delim + 1, name.length() - 1); // index = 0
+      name = name.substring(0, delim); // name = orders
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getIndex() {
+    return index;
+  }
+
+  public String getIndexedName() {
+    return indexedName;
+  }
+
+  public String getChildren() {
+    return children;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return children != null;
+  }
+
+  @Override
+  public PropertyTokenizer next() {
+    return new PropertyTokenizer(children);
+  }
+
+  @Override
+  public void remove() {
+    throw new UnsupportedOperationException("Remove is not supported, as it has no meaning in the context of properties.");
+  }
+}
